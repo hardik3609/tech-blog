4fa8a42c5b1def422529fbe6bd770f7bab5a57d0
hardik3609 <hardikkumawat3985@gmail.com>
2018-08-06 12:12:30 +0530

    Adding code of homepage

---
+++ backup.php
@@ -0,0 +1,325 @@
+<?php include"header.php";?>
+    <!-- s-content
+    ================================================== -->
+    <section class="s-content">
+        
+        <div class="row masonry-wrap">
+            <div class="masonry">
+
+                <div class="grid-sizer"></div>
+				
+				<div class="col-md-8">
+					<!--single block start-->
+					<div class="top-blog">
+						<div class="top-blog-image">
+							<a href="#">
+								<img src="images/thumbs/masonry/blog1.jpg" srcset="images/thumbs/masonry/blog1.jpg 1x, images/thumbs/masonry/blog1.jpg 2x" alt="">
+							</a>
+						</div>
+						<div class="top-blog-content">
+							<center>
+							<h2>Girl play with fireworks in nature</h2>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+							the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of typ
+							e and scrambled it to make a type specimen book Lorem Ipsum has been the in…
+							</p>
+							</center>
+						</div>
+					</div>
+					<!--single block end-->
+					<!--Other blog start-->
+					<div class="row">
+						<!--blog start-->
+						<div class="clearfix visible-xs"></div>
+						<div class="col-md-6">
+							<article class=" entry format-standard" data-aos="fade-up">
+								<div class="entry__thumb">
+									<a href="single-standard.html" class="entry__thumb-link">
+										<img src="images/thumbs/masonry/lamp-400.jpg" 
+												srcset="images/thumbs/masonry/lamp-400.jpg 1x, images/thumbs/masonry/lamp-800.jpg 2x" alt="">
+									</a>
+								</div>
+
+								<div class="entry__text">
+									<div class="entry__header">
+										<div class="entry__meta">
+											<span class="entry__meta-links">
+												<a href="#">Design</a> 
+											</span>
+										</div>
+										<h1 class="entry__title"><a href="#">Just a Standard Format Post.Just a Standard Format Post.</a></h1>
+										
+									</div>
+									<div class="entry__excerpt">
+										<p>
+											Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua...
+										</p>
+									</div>
+									<div class="entry__date">
+										<a href="#">December 15, 2017</a>
+									</div>
+									<div>
+										<button type="button" class="submit btn btn--primary full-width">Read More</button>
+									</div>
+								</div>
+							</article> <!-- end article -->
+						</div>
+						
+						<!--blog end-->
+						<!--blog start-->
+						<div class="col-md-6">
+							<article class=" entry format-standard" data-aos="fade-up">
+								<div class="entry__thumb">
+									<a href="single-standard.html" class="entry__thumb-link">
+										<img src="images/thumbs/masonry/lamp-400.jpg" 
+												srcset="images/thumbs/masonry/lamp-400.jpg 1x, images/thumbs/masonry/lamp-800.jpg 2x" alt="">
+									</a>
+								</div>
+
+								<div class="entry__text">
+									<div class="entry__header">
+										<div class="entry__meta">
+											<span class="entry__meta-links">
+												<a href="#">Design</a> 
+											</span>
+										</div>
+										<h1 class="entry__title"><a href="#">Just a Standard Format Post.</a></h1>
+										
+									</div>
+									<div class="entry__excerpt">
+										<p>
+											Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua...
+										</p>
+									</div>
+									<div class="entry__date">
+										<a href="#">December 15, 2017</a>
+									</div>
+									<div>
+										<button type="button" class="submit btn btn--primary full-width">Read More</button>
+									</div>
+								</div>
+							</article> <!-- end article -->
+						</div>
+						<!--blog end-->
+						<!--blog start-->
+						<div class="col-md-6">
+							<article class=" entry format-standard" data-aos="fade-up">
+								<div class="entry__thumb">
+									<a href="single-standard.html" class="entry__thumb-link">
+										<img src="images/thumbs/masonry/lamp-400.jpg" 
+												srcset="images/thumbs/masonry/lamp-400.jpg 1x, images/thumbs/masonry/lamp-800.jpg 2x" alt="">
+									</a>
+								</div>
+
+								<div class="entry__text">
+									<div class="entry__header">
+										<div class="entry__meta">
+											<span class="entry__meta-links">
+												<a href="#">Design</a> 
+											</span>
+										</div>
+										<h1 class="entry__title"><a href="#">Just a Standard Format Post.Just a Standard Format Post.</a></h1>
+										
+									</div>
+									<div class="entry__excerpt">
+										<p>
+											Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua...
+										</p>
+									</div>
+									<div class="entry__date">
+										<a href="#">December 15, 2017</a>
+									</div>
+									<div>
+										<button type="button" class="submit btn btn--primary full-width">Read More</button>
+									</div>
+								</div>
+							</article> <!-- end article -->
+						</div>
+						<!--blog end-->
+						<!--blog start-->
+						<div class="col-md-6">
+							<article class=" entry format-standard" data-aos="fade-up">
+								<div class="entry__thumb">
+									<a href="single-standard.html" class="entry__thumb-link">
+										<img src="images/thumbs/masonry/lamp-400.jpg" 
+												srcset="images/thumbs/masonry/lamp-400.jpg 1x, images/thumbs/masonry/lamp-800.jpg 2x" alt="">
+									</a>
+								</div>
+
+								<div class="entry__text">
+									<div class="entry__header">
+										<div class="entry__meta">
+											<span class="entry__meta-links">
+												<a href="#">Design</a> 
+											</span>
+										</div>
+										<h1 class="entry__title"><a href="#">Just a Standard Format Post.Just a Standard Format Post.</a></h1>
+										
+									</div>
+									<div class="entry__excerpt">
+										<p>
+											Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua...
+										</p>
+									</div>
+									<div class="entry__date">
+										<a href="#">December 15, 2017</a>
+									</div>
+									<div>
+										<button type="button" class="submit btn btn--primary full-width">Read More</button>
+									</div>
+								</div>
+							</article> <!-- end article -->
+						</div>
+						<!--blog end-->
+					</div>
+					<div class="clearfix">...</div>
+					<!--Other blog end-->
+				</div>
+				<div class="col-md-4">.col-md-4</div>
+
+            </div> <!-- end masonry -->
+        </div> <!-- end masonry-wrap -->
+
+        <div class="row">
+            <div class="col-full">
+                <nav class="pgn">
+                    <ul>
+                        <li><a class="pgn__prev" href="#0">Prev</a></li>
+                        <li><a class="pgn__num" href="#0">1</a></li>
+                        <li><span class="pgn__num current">2</span></li>
+                        <li><a class="pgn__num" href="#0">3</a></li>
+                        <li><a class="pgn__num" href="#0">4</a></li>
+                        <li><a class="pgn__num" href="#0">5</a></li>
+                        <li><span class="pgn__num dots">…</span></li>
+                        <li><a class="pgn__num" href="#0">8</a></li>
+                        <li><a class="pgn__next" href="#0">Next</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+    </section> <!-- s-content -->
+
+
+    <!-- s-extra
+    ================================================== -->
+    <section class="s-extra">
+
+        <div class="row top">
+
+            <div class="col-eight md-six tab-full popular">
+                <h3>Popular Posts</h3>
+
+                <div class="block-1-2 block-m-full popular__posts">
+                    <article class="col-block popular__post">
+                        <a href="#0" class="popular__thumb">
+                            <img src="images/thumbs/small/wheel-150.jpg" alt="">
+                        </a>
+                        <h5><a href="#0">Visiting Theme Parks Improves Your Health.</a></h5>
+                        <section class="popular__meta">
+                                <span class="popular__author"><span>By</span> <a href="#0"> John Doe</a></span>
+                            <span class="popular__date"><span>on</span> <time datetime="2017-12-19">Dec 19, 2017</time></span>
+                        </section>
+                    </article>
+                    <article class="col-block popular__post">
+                        <a href="#0" class="popular__thumb">
+                            <img src="images/thumbs/small/shutterbug-150.jpg" alt="">
+                        </a>
+                        <h5><a href="#0">Key Benefits Of Family Photography.</a></h5>
+                        <section class="popular__meta">
+                            <span class="popular__author"><span>By</span> <a href="#0"> John Doe</a></span>
+                            <span class="popular__date"><span>on</span> <time datetime="2017-12-18">Dec 18, 2017</time></span>
+                        </section>
+                    </article>
+                    <article class="col-block popular__post">
+                        <a href="#0" class="popular__thumb">
+                            <img src="images/thumbs/small/cookies-150.jpg" alt="">
+                        </a>
+                        <h5><a href="#0">Absolutely No Sugar Oatmeal Cookies.</a></h5>
+                        <section class="popular__meta">
+                                <span class="popular__author"><span>By</span> <a href="#0"> John Doe</a></span>
+                            <span class="popular__date"><span>on</span> <time datetime="2017-12-16">Dec 16, 2017</time></span>
+                        </section>
+                    </article>
+                    <article class="col-block popular__post">
+                        <a href="#0" class="popular__thumb">
+                            <img src="images/thumbs/small/beetle-150.jpg" alt="">
+                        </a>
+                        <h5><a href="#0">Throwback To The Good Old Days.</a></h5>
+                        <section class="popular__meta">
+                            <span class="popular__author"><span>By</span> <a href="#0"> John Doe</a></span>
+                            <span class="popular__date"><span>on</span> <time datetime="2017-12-16">Dec 16, 2017</time></span>
+                        </section>
+                    </article>
+                    <article class="col-block popular__post">
+                        <a href="#0" class="popular__thumb">
+                            <img src="images/thumbs/small/tulips-150.jpg" alt="">
+                        </a>
+                        <h5><a href="#0">10 Interesting Facts About Caffeine.</a></h5>
+                        <section class="popular__meta">
+                            <span class="popular__author"><span>By</span> <a href="#0"> John Doe</a></span>
+                            <span class="popular__date"><span>on</span> <time datetime="2017-12-14">Dec 14, 2017</time></span>
+                        </section>
+                    </article>
+                    <article class="col-block popular__post">
+                        <a href="#0" class="popular__thumb">
+                            <img src="images/thumbs/small/salad-150.jpg" alt="">
+                        </a>
+                        <h5><a href="#0">Healthy Mediterranean Salad Recipes</a></h5>
+                        <section class="popular__meta">
+                            <span class="popular__author"><span>By</span> <a href="#0"> John Doe</a></span>
+                            <span class="popular__date"><span>on</span> <time datetime="2017-12-12">Dec 12, 2017</time></span>
+                        </section>
+                    </article>
+                </div> <!-- end popular_posts -->
+            </div> <!-- end popular -->
+            
+            <div class="col-four md-six tab-full about">
+                <h3>About Philosophy</h3>
+
+                <p>
+                Donec sollicitudin molestie malesuada. Nulla quis lorem ut libero malesuada feugiat. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada.
+                </p>
+
+                <ul class="about__social">
+                    <li>
+                        <a href="#0"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="#0"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="#0"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="#0"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                    </li>
+                </ul> <!-- end header__social -->
+            </div> <!-- end about -->
+
+        </div> <!-- end row -->
+
+        <div class="row bottom tags-wrap">
+            <div class="col-full tags">
+                <h3>Tags</h3>
+
+                <div class="tagcloud">
+                    <a href="#0">Salad</a>
+                    <a href="#0">Recipe</a>
+                    <a href="#0">Places</a>
+                    <a href="#0">Tips</a>
+                    <a href="#0">Friends</a>
+                    <a href="#0">Travel</a>
+                    <a href="#0">Exercise</a>
+                    <a href="#0">Reading</a>
+                    <a href="#0">Running</a>
+                    <a href="#0">Self-Help</a>
+                    <a href="#0">Vacation</a>
+                </div> <!-- end tagcloud -->
+            </div> <!-- end tags -->
+        </div> <!-- end tags-wrap -->
+
+    </section> <!-- end s-extra -->
+
+
+<?php include"footer.php";?>
---
+++ footer.php
@@ -0,0 +1,154 @@
+    <!-- s-footer
+    ================================================== -->
+    <footer class="s-footer">
+		<div class="container">
+			<div class="s-footer__main">
+				<div class="row">
+					
+					<div class="col-md-2 s-footer__sitelinks">
+							
+						<h4 class="text-center">Quick Links</h4>
+
+						<ul class="s-footer__linklist">
+							<li><a href="#0">Home</a></li>
+							<li><a href="#0">Blog</a></li>
+							<li><a href="#0">Styles</a></li>
+							<li><a href="#0">About</a></li>
+							<li><a href="#0">Contact</a></li>
+							<li><a href="#0">Privacy Policy</a></li>
+						</ul>
+
+					</div> <!-- end s-footer__sitelinks -->
+
+					
+
+					<div class="col-md-2 s-footer__social">
+							
+						<h4 class="text-center">Social</h4>
+
+						<ul class="s-footer__linklist">
+							<li><a href="#0">Facebook</a></li>
+							<li><a href="#0">Instagram</a></li>
+							<li><a href="#0">Twitter</a></li>
+							<li><a href="#0">Pinterest</a></li>
+							<li><a href="#0">Google+</a></li>
+							<li><a href="#0">LinkedIn</a></li>
+						</ul>
+
+					</div> <!-- end s-footer__social -->
+					<div class="col-md-2 s-footer__social">
+							
+						<h4 class="text-center">Social</h4>
+
+						<ul class="s-footer__linklist">
+							<li><a href="#0">Facebook</a></li>
+							<li><a href="#0">Instagram</a></li>
+							<li><a href="#0">Twitter</a></li>
+							<li><a href="#0">Pinterest</a></li>
+							<li><a href="#0">Google+</a></li>
+							<li><a href="#0">LinkedIn</a></li>
+						</ul>
+
+					</div> <!-- end s-footer__social -->
+
+					<div class="col-md-6 s-footer__subscribe">
+							
+						<h4>Our Newsletter</h4>
+
+						<p>Sit vel delectus amet officiis repudiandae est voluptatem. Tempora maxime provident nisi et fuga et enim exercitationem ipsam. Culpa consequatur occaecati.</p>
+
+						<div class="subscribe-form">
+							<form id="mc-form" class="group" novalidate="true">
+
+								<input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required="">
+					
+								<input type="submit" name="subscribe" value="Send">
+					
+								<label for="mc-email" class="subscribe-message"></label>
+					
+							</form>
+						</div>
+
+					</div> <!-- end s-footer__subscribe -->
+
+				</div>
+			</div> <!-- end s-footer__main -->
+
+			<div class="s-footer__bottom">
+				<div class="container">
+					<div class="row">
+						<div class="col-full text-center">
+							<div class="s-footer__copyright">
+								<span>© Copyright Ronak Taldar</span> 
+								<span>Design by <a href="https://www.taldartechconsultancy.com" target="_blank" >Taldar Tech Consultancy</a></span>
+							</div>
+
+							<div class="go-top">
+								<a class="smoothscroll" title="Back to Top" href="#top"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> <!-- end s-footer__bottom -->
+		</div>
+
+    </footer> <!-- end s-footer -->
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Java Script
+    ================================================== -->
+    
+    <script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+<!--Auto set align blog-->
+	<script>
+function resizeGridItem(item){
+  grid = document.getElementsByClassName("grid")[0];
+  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+    item.style.gridRowEnd = "span "+rowSpan;
+}
+
+function resizeAllGridItems(){
+  allItems = document.getElementsByClassName("item");
+  for(x=0;x<allItems.length;x++){
+    resizeGridItem(allItems[x]);
+  }
+}
+
+function resizeInstance(instance){
+    item = instance.elements[0];
+  resizeGridItem(item);
+}
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
+allItems = document.getElementsByClassName("item");
+for(x=0;x<allItems.length;x++){
+  imagesLoaded( allItems[x], resizeInstance);
+}
+	</script>
+<!--//Auto set align blog-->
+</body>
+
+</html>
